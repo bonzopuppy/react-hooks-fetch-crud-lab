771500f15bd2df87a69ad285de6692e1055c4734
David Baden <davebaden@gmail.com>
2023-11-27 23:07:17 -0800

    get and post

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questionList, setQuestionlist] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((questions) => setQuestionlist(questions));
+  }, []);
+
+  function handleNewQuestion(newQuestion) {
+    setQuestionlist([...questionList, newQuestion]);
+   }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm onNewQuestion={handleNewQuestion} /> : <QuestionList questions={questionList}/>}
     </main>
   );
 }
